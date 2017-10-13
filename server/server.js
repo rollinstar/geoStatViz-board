995842330d6f581d4e610b0ec3d4c95baa5157c8
SangHwan Jun <linkit@SangHwanui-MacBook-Pro.local>
2017-10-13 18:33:04 +0900

    create node server environment

--- server/server.js
+++ server/server.js
@@ -1,9 +1,33 @@
-var express = require('express')
-    app     = express();
+var express    = require('express'),
+    bodyParser = require('body-parser'),
+    path       = require('path'),
+    app        = express();
+    
 
-
-//configuration
-app.configure(function(){
-    // app.use("/index.html",express.static(__dirname+"/index.html"));
-    app.use("/index.html",express.static("../app/index.html"));
+// Middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');                       //허용되는 url을 route를 제외하고 명시하면 이외의 url로 부터 유입되는 request는 거절된다. (*은 모든 request 허가)
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); //허용되는 HTTP methods, 포함되지 않은 method는 거절된다. (* 사용불가)
+  res.header('Access-Control-Allow-Headers', 'content-type');           //허용되는 HTTP header 목록 (* 사용불가)
+  next();
 });
+
+// APIs
+app.use('/api/test', require('./api/test')); //*
+
+// Angular
+app.use(express.static(path.resolve(__dirname, '../build'))); //1
+app.get('*', function (req, res) { //2
+  var indexFile = path.resolve(__dirname,'../build/index.html');
+  res.sendFile(indexFile);
+});
+
+// Server
+var port = 8080;
+app.listen(port, function(){
+  console.log('listening on port:' + port);
+});
+
+console.log("App listening on port 8080");
