99203fee7da975d8f8460593f3d745d94a8e13b2
SangHwan Jun <linkit@SangHwanui-MacBook-Pro.local>
2017-10-16 18:40:00 +0900

    test R-script

--- server/api/test.js
+++ server/api/test.js
@@ -3,6 +3,8 @@
  */
 let express = require('express');
 let router  = express.Router();
+let R = require("r-script");
+let fs = require("fs");
 
 router.get('/:test',
     function(req, res, next){
@@ -19,8 +21,10 @@ router.get('/:test',
     }
 );
 
-router.get('/:test2',
+router.get('/rtest/:test2',
     function(req, res, next){
+        console.log('req : ', req.params.test2);
+        console.log('dir name : ', __dirname);
         let result = {
             success: true,
             data: {
@@ -28,6 +32,15 @@ router.get('/:test2',
                 name: 'test name'
             }
         };
+
+        var test = JSON.parse(fs.readFileSync(__dirname + "/test.json", "utf8"));
+        
+        R("/Users/linkit/Documents/00 Dev/linkit-geo-board/server/rscript/test.R")
+        .data({df: test, nGroups: 3, fxn: "mean" })
+        .call(function(err, d) {
+            if (err) throw err;
+            console.log('test : ', d);
+        });
 
         res.json(result);
     }
---
+++ server/rscript/test.R
@@ -0,0 +1,18 @@
+# ex-async.R
+print("test 1111")
+# needs(dplyr)
+# print("test 2222")
+# attach(input[[1]])
+# print("test 3333")
+# return("early returns are ignored")
+# cat("so are undirected calls to cat")
+# print("or print")
+# cat("unless directed to a file", file = "out.Rout")
+
+# # output of final expression is returned to parent
+# df %>% 
+#   mutate(group = cut(rating, nGroups, ordered = T)) %>% 
+#   group_by(group) %>% 
+#   summarize_each(funs_(fxn)) %>%
+#   select(group, rating, advance) %>%
+#   mutate(group = as.character(group))
